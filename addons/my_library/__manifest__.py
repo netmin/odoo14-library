10be548d7fc2ca10de63db3fb7b720911424b988
Vitaly D <admin@netmin.ru>
2020-10-17 15:24:36 +0500

    create manifest

---
+++ addons/my_library/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'My Library',
+    'summary': 'Manage books easily',
+    'description': 'My library books',
+    'author': 'Vital D.',
+    'website': 'netmin.ru',
+    'category': 'Uncategorized',
+    'version': '14.0.1',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views.xml',
+    ],
+    'demo': []
+}
