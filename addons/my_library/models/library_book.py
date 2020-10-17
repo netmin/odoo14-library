aaa60214d61eae5906f618ba7a9d1d5327010047
Vitaly D <admin@netmin.ru>
2020-10-17 19:16:33 +0500

    fix data field for library book model

--- addons/my_library/models/library_book.py
+++ addons/my_library/models/library_book.py
@@ -8,7 +8,12 @@ class LibraryBook(models.Model):
     _rec_name = "short_name"
 
     name = fields.Char("Title", required=True)
-    short_name = fields.Char("Short Title", required=True)
+    short_name = fields.Char(
+        "Short Title",
+        required=True,
+        translate=True,
+        index=True,
+    )
     notes = fields.Text("Internal Notes")
     state = fields.Selection(
         [
@@ -17,12 +22,19 @@ class LibraryBook(models.Model):
             ("lost", "Lost"),
         ],
         "State",
+        default='draft',
     )
-    description = fields.Html("Description")
+    description = fields.Html("Description", sanitize=True, strip_style=False)
     cover = fields.Binary("Book Cover")
     out_of_print = fields.Boolean("Out of Print?")
     date_release = fields.Date("Release Date")
     date_updated = fields.Datetime("Last Updated")
-    pages = fields.Integer("Number of Pages")
+    pages = fields.Integer(
+        "Number of Pages",
+        groups='base.group_user',
+        states={'lost': [('readonly', True)]},
+        help='Total book page count',
+        company_dependent=False,
+    )
     reader_rating = fields.Float("Reader Average Rating", digits=(14, 4))
     author_ids = fields.Many2many("res.partner", string="Authors")
