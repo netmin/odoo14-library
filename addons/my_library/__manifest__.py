0ed0873727393b067c15240b4fe569b3b523cd69
Vitaly D <admin@netmin.ru>
2020-10-17 18:03:21 +0500

    black formatting

--- addons/my_library/__manifest__.py
+++ addons/my_library/__manifest__.py
@@ -1,18 +1,18 @@
 {
-    'name': 'My Library',
-    'summary': 'Manage books easily',
-    'description': 'My library books',
-    'author': 'Vital D.',
-    'website': 'netmin.ru',
-    'category': 'Uncategorized',
-    'version': '14.0.1',
-    'depends': [
-        'base',
+    "name": "My Library",
+    "summary": "Manage books easily",
+    "description": "My library books",
+    "author": "Vital D.",
+    "website": "netmin.ru",
+    "category": "Uncategorized",
+    "version": "14.0.1",
+    "depends": [
+        "base",
     ],
-    'data': [
-        'security/groups.xml',
-        'security/ir.model.access.csv',
-        'views/library_book_views.xml',
+    "data": [
+        "security/groups.xml",
+        "security/ir.model.access.csv",
+        "views/library_book_views.xml",
     ],
-    'demo': []
+    "demo": [],
 }
--- addons/my_library/models/library_book.py
+++ addons/my_library/models/library_book.py
@@ -2,8 +2,8 @@ from odoo import fields, models
 
 
 class LibraryBook(models.Model):
-    _name = 'library.book'
+    _name = "library.book"
 
-    name = fields.Char('Title', required=True)
-    date_release = fields.Date('Release Date')
-    author_ids = fields.Many2many('res.partner', string='Authors')
+    name = fields.Char("Title", required=True)
+    date_release = fields.Date("Release Date")
+    author_ids = fields.Many2many("res.partner", string="Authors")
