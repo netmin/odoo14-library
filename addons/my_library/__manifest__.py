7211bdf792a2586e4fc68825d9da65bd619c59be
Vitaly D <admin@netmin.ru>
2020-10-17 17:32:55 +0500

    create access rule and fix view for library book

--- addons/my_library/__manifest__.py
+++ addons/my_library/__manifest__.py
@@ -10,7 +10,9 @@
         'base',
     ],
     'data': [
-        'library_book_views.xml',
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book_views.xml',
     ],
     'demo': []
 }
