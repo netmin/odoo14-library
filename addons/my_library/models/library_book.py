f131d5ccb3477c65698a370c99f29d442013638d
Vitaly D <admin@netmin.ru>
2020-10-17 18:47:33 +0500

    black formatting

--- addons/my_library/models/library_book.py
+++ addons/my_library/models/library_book.py
@@ -3,26 +3,26 @@ from odoo import fields, models
 
 class LibraryBook(models.Model):
     _name = "library.book"
-    _description = 'Library Book'
-    _order = 'date_release desc, name'
-    _rec_name = 'short_name'
+    _description = "Library Book"
+    _order = "date_release desc, name"
+    _rec_name = "short_name"
 
     name = fields.Char("Title", required=True)
-    short_name = fields.Char('Short Title', required=True)
-    notes = fields.Text('Internal Notes')
+    short_name = fields.Char("Short Title", required=True)
+    notes = fields.Text("Internal Notes")
     state = fields.Selection(
         [
-            ('draft', 'Not Available'),
-            ('available', 'Available'),
-            ('lost', 'Lost'),
+            ("draft", "Not Available"),
+            ("available", "Available"),
+            ("lost", "Lost"),
         ],
-        'State'
+        "State",
     )
-    description = fields.Html('Description')
-    cover = fields.Binary('Book Cover')
-    out_of_print = fields.Boolean('Out of Print?')
+    description = fields.Html("Description")
+    cover = fields.Binary("Book Cover")
+    out_of_print = fields.Boolean("Out of Print?")
     date_release = fields.Date("Release Date")
-    date_updated = fields.Datetime('Last Updated')
-    pages = fields.Integer('Number of Pages')
-    reader_rating = fields.Float('Reader Average Rating', digits=(14, 4))
+    date_updated = fields.Datetime("Last Updated")
+    pages = fields.Integer("Number of Pages")
+    reader_rating = fields.Float("Reader Average Rating", digits=(14, 4))
     author_ids = fields.Many2many("res.partner", string="Authors")
