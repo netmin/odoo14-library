69400c4fdb1ff8dcb1b50ba45413f479817273d6
Vitaly D <admin@netmin.ru>
2020-10-17 18:47:13 +0500

    add data field for book in library book model and view

--- addons/my_library/models/library_book.py
+++ addons/my_library/models/library_book.py
@@ -9,5 +9,20 @@ class LibraryBook(models.Model):
 
     name = fields.Char("Title", required=True)
     short_name = fields.Char('Short Title', required=True)
+    notes = fields.Text('Internal Notes')
+    state = fields.Selection(
+        [
+            ('draft', 'Not Available'),
+            ('available', 'Available'),
+            ('lost', 'Lost'),
+        ],
+        'State'
+    )
+    description = fields.Html('Description')
+    cover = fields.Binary('Book Cover')
+    out_of_print = fields.Boolean('Out of Print?')
     date_release = fields.Date("Release Date")
+    date_updated = fields.Datetime('Last Updated')
+    pages = fields.Integer('Number of Pages')
+    reader_rating = fields.Float('Reader Average Rating', digits=(14, 4))
     author_ids = fields.Many2many("res.partner", string="Authors")
